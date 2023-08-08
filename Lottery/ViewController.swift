81644d81865c290c77fc1b19c855d7d428f933c2
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-08 15:41:57 +0900

    Initial Commit

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by 서동운 on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
